eed7774299125d448da579787f0e24c1a877235b
Vinag S V <119550290+vinagsv@users.noreply.github.com>
2025-03-20 20:45:05 +0530

    updated server code

--- todo_level1/server/server.js
+++ todo_level1/server/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import connectDb from "./utils/db.js";
 import router from "./routes/tasks.route.js";
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
